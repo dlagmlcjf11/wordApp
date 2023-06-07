a7c84563c868dbcdc59e628d5314afba93740827
dlagmlcjf11 <92962046+dlagmlcjf11@users.noreply.github.com>
2023-06-07 12:50:24 +0900

    mainContent

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -22,24 +23,122 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.amber,
       ),
       home: mainPage(),
     );
   }
 }
 
-class mainPage extends StatelessWidget {
+class mainPage extends StatefulWidget {
   const mainPage({Key? key}) : super(key: key);
+
+  @override
+  State<mainPage> createState() => _mainPageState();
+}
+
+class _mainPageState extends State<mainPage> {
+  int _selectedIndex = 0;
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('제목'),
+        backgroundColor: Colors.amberAccent,
+        title: Text(
+          'title',
+          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+        ),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          _topContent(),
+          _middleContent(),
+          _bottomContent(),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.article_outlined),
+            label: '단어장',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add),
+            label: 'ㄴㄴ',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add),
+            label: 'ㄴㄴ',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.black,
+        onTap: _onItemTapped,
       ),
     );
   }
 }
 
+Widget _topContent() {
+  return Padding(
+    padding: const EdgeInsets.all(8.0),
+    child: Column(
+      children: [
+        Container(
+          width: 400,
+          height: 60,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(6),
+            border: Border.all(
+              width: 1,
+            ),
+            shape: BoxShape.rectangle,
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.18),
+                blurRadius: 5.0,
+                spreadRadius: 0.0,
+                offset: const Offset(0, 7),
+              ),
+            ],
+          ),
+          child: TextButton(
+              onPressed: () {},
+              child: Row(
+                children: [
+                  Icon(
+                    Icons.add,
+                    color: Colors.black,
+                  ),
+                  SizedBox(
+                    width: 5,
+                  ),
+                  Text(
+                    '단어 추가하기',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              )),
+        ),
+      ],
+    ),
+  );
+}
 
+Widget _middleContent() {
+  return Text('middle');
+}
+
+Widget _bottomContent() {
+  return Text('bottom');
+}
