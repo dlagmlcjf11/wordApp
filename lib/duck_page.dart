8bb7bb18cd327a4eb1c5c90e2c31a1a2fd7bc014
dlagmlcjf11 <92962046+dlagmlcjf11@users.noreply.github.com>
2023-06-08 16:12:42 +0900

    dsadas

--- lib/duck_page.dart
+++ lib/duck_page.dart
@@ -21,12 +21,54 @@ class MyApp extends StatelessWidget {
 }
 
 class DuckPage extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('duck'),
+        title: Text(
+          '오리',
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Container(
+                width: 350,
+                height: 350,
+                decoration: BoxDecoration(
+                  border: Border.all(width: 1),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Center(
+                  child: Image.asset('assets/duck_first.png'),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 5,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Text('Lv.1 노래 듣는 오리', style: TextStyle(fontWeight: FontWeight.bold),),
+                Text('경험치: 0', style: TextStyle(fontWeight: FontWeight.bold),),
+              ],
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text(
+                '밥 주기',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/wordtest_page.dart
+++ lib/wordtest_page.dart
@@ -21,12 +21,53 @@ class MyApp extends StatelessWidget {
 }
 
 class WordTestPage extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('test'),
+        title: Text(
+          '단어 퀴즈',
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Container(
+                width: 350,
+                height: 350,
+                decoration: BoxDecoration(
+                  border: Border.all(width: 1),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Center(
+                  child: Text(
+                    '단어',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 30,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: TextField(
+                decoration: InputDecoration(hintText: '단어의 뜻을 입력하세요'),
+              ),
+            ),
+            SizedBox(
+              height: 5,
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text('확인', style: TextStyle(fontWeight: FontWeight.bold),),
+            ),
+          ],
+        ),
       ),
     );
   }
